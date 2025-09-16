329b808036d11a84381de00fb9e4fe8df589c482
RRMermaid <rnizametdinova1991@gmail.com>
2025-09-17 01:45:12 +0500

    добавила поисковик айди бота

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN, CHAT_ID
+from aiogram import types
+from telegram_bot import dp
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -10,3 +12,9 @@ return invite.invite_link
 
 async def revoke_access(user_id: int):
 await bot.kick_chat_member(CHAT_ID, user_id)
+
+@dp.message_handler()
+async def get_chat_id(message: types.Message):
+    await message.reply(f"Chat ID: {message.chat.id}")
+    print(f"Chat ID: {message.chat.id}")
+    
